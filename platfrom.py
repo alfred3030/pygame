bfc11955029e702839eac105efe13307f291e5df
alfred3030 <alfred1020125@gmail.com>
2025-10-07 18:24:53 +0800

    pygame_plat1.1
    地圖.圖片

--- platfrom.py
+++ platfrom.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
 #define game variadles
-tile_size = 50
+tile_size = 200
 
 
 # 載入並縮放圖片
@@ -24,17 +24,44 @@ def draw_grid():
 
 
 
-class Worls():
+class World():
       def __init__(self, data):
+            self.tile_list = []
             
             #load images
             dirt_img = pygame.image.load('assets/img/dirt.png')
 
+            row_count = 0
+            for row in data:
+                col_count = 0
+                for tile in row:
+                    if tile == 1:
+                        img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                        img_rect = img.get_rect()
+                        img_rect.x = col_count * tile_size
+                        img_rect.y = row_count * tile_size
+                        tile = (img, img_rect)
+                        self.tile_list.append(tile)
+                    col_count += 1
+                row_count += 1
+
+      def draw(self):
+            for tile in self.tile_list:
+                screen.blit(tile[0], tile[1])
 
 
-world_data = [
 
+world_data = [      
+[1, 1, 1, 1, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 1, 1, 1, 1],
 ]
+
+
+world = World(world_data)
+
 # 遊戲迴圈
 run = True
 while run:
@@ -42,6 +69,8 @@ while run:
     # 畫背景和太陽
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
+
+    world.draw()
 
     draw_grid()
 

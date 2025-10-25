6c1bbec8d97ab9c946d1b206f7cbaa33899b830b
alfred3030 <alfred1020125@gmail.com>
2025-10-25 11:03:40 +0800

    2025-10-25 v1.6
    platform.pyplayer move animation 100%

--- platform.py
+++ platform.py
@@ -25,23 +25,29 @@ sun_img = pygame.image.load('assets/img/sun.png')
 class Player():
      def __init__(self, x, y):
           self.images_right = []
+          self.images_left = []
           self.index = 0
           self.counter = 0
           for num in range(1, 5):
               img_right = pygame.image.load(f'assets/img/guy{num}.png')
               img_right = pygame.transform.scale(img_right, (40, 80))
+              img_left = pygame.transform.flip(img_right, True, False)
               self.images_right.append(img_right)
+              self.images_left.append(img_left)
           self.image = self.images_right[self.index]
           self.rect = self.image.get_rect()
           self.rect.x = x
           self.rect.y = y
           self.vel_y = 0
           self.jumped = False
+          self.direction = 0
 
-     def update(self):
-          
+
+     def update(self):          
           dx = 0
           dy = 0
+          walk_cooldown = 5
+
 
           #get keypresses
           key = pygame.key.get_pressed()
@@ -52,13 +58,33 @@ class Player():
                self.jumped = False
           if key[pygame.K_LEFT]:
                dx -= 5
+               self.counter += 1
+               self.direction = -1
           if key[pygame.K_RIGHT]:
                dx += 5
+               self.counter += 1
+               self.direction = 1
+          if key[pygame.K_LEFT] ==False and key[pygame.K_RIGHT] == False:
+               self.counter = 0
+               self.index = 0
+               if self.direction == 1:
+                    self.image = self.images_right[self.index]
+               if self.direction == -1:
+                    self.image = self.images_left[self.index]
 
-          self.index += 1
-          if self.index >= len(self.images_right):
-              self.index = 0
-          self.images_right[self.index]
+
+          #handle animation
+          if self.counter > walk_cooldown:
+                self.counter = 0
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
 
           #add gravity
           self.vel_y += 1
